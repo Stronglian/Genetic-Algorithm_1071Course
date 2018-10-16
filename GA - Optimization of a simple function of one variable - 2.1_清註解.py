d0f94ded74ab77a6e1cd2bd4230d9a0849ed55b9
Stronglian <strong.85.wu@gmail.com>
2018-10-16 19:38:14 +0800

    update
    清一個乾淨的

--- GA - Optimization of a simple function of one variable - 2.1_清註解.py
+++ GA - Optimization of a simple function of one variable - 2.1_清註解.py
@@ -11,6 +11,10 @@ Created on Fri Oct  5 02:51:31 2018
       - 針對最小樹小於零的數列，直接加最小值的絕對值
      -2. fitness 儲存格改成 float
      -3. 是 隨機數字小於變異率(交配) 才會變異(交配)
+    2. 字串運算
+     - 提升效率，字串、過去未知函數
+     - 補充備註
+     - random vs np.random
     *. (下一站)，'101' 改成 [1,0,1]
     *. (下一站)，跑到趨緩在停
      - self.repeatGeneration FREE
@@ -19,10 +23,9 @@ Created on Fri Oct  5 02:51:31 2018
 reference:
     
 """
-
 import numpy as np
 import random
-
+#random.seed('111')
 class GeneticAlgorithm():
     def __init__(self):
         """
@@ -57,11 +60,11 @@ class GeneticAlgorithm():
         return bitString
     def CalBitValue(self, bitString):
         """計算二位元轉十進位"""
-        sumNum = 0
-        for i, bit in enumerate(bitString[::-1]):#倒序
-            bit = int(bit)
-            sumNum += (2**i)*bit
-        return sumNum
+#        sumNum = 0
+#        for i, bit in enumerate(bitString[::-1]):#倒序
+#            bit = int(bit)
+#            sumNum += (2**i)*bit
+        return int(bitString, 2)#sumNum
     def CalCorrespondValue(self, bitString):
         """將 x 轉換到對應的值˙"""
         x_ = self.CalBitValue(bitString)
@@ -72,16 +75,16 @@ class GeneticAlgorithm():
         也可以不用機率直接用 ranTmp = random.randint(0, sumWeight)做處理，但比較慢。
         不重複取
         """
-        weightArr = (fitnessArr.copy().astype(float))+1
+        weightArr = (fitnessArr.copy().astype(float)) +1 #處理0的問題
         pairGroup = [[] for i in range(len(inputArr))] #配對紀錄
         #計算輪盤 #挑選、分組
-        pairNum = 0
-        pairNumCount = 0
-        if weightArr.min() < 0:
+        pairNum = 0 #已有的配對數量
+        pairNumCount = 0 #該配對有幾個成員(bitString)
+        if weightArr.min() < 0: #若有值小於0就把所有值加上它的絕對值。
             weightArr += np.absolute(weightArr.min())
         while pairNum < self.crossoverPair:
             ranTmp = random.uniform(0, weightArr.sum())
-            for i in range(len(weightArr)):
+            for i in range(len(weightArr)): #輪盤每一格的判斷
                 if ranTmp < sum(weightArr[:i+1]):
                     #控制重複取與否
                     if len(pairGroup[i]) != 0 and self.__wheelGetDiffPopOnlyTF__:
@@ -108,40 +111,39 @@ class GeneticAlgorithm():
         for i in range(self.crossoverPair): #第幾對
 #            print(i, '---')
             #找配對的
-            j = 0
-            while j != self.tournamentSize:
+            pairNumCount = 0
+            while pairNumCount != self.tournamentSize:
                 for k, pairNumLi in enumerate(pairGroup):
                     if i in pairNumLi:
-                        tmpPairLi[j] = k
-                        j += 1
-                    if j == self.tournamentSize:
+                        tmpPairLi[pairNumCount] = k
+                        pairNumCount += 1
+                    if pairNumCount == self.tournamentSize:
                         break
 #            print(tmpPairLi)
             #配對與否
             if random.random() < self.crossoverRate:
                 #one-point 交換點
-                crossoverPonint = random.randint(0, self.bitNum)
+                crossoverPonint = random.randint(1, self.bitNum) #= 0 的時候不就沒配對了
                 tmpStrLi = ['' for i in range(self.tournamentSize)] #暫存交換的String
-                for j in range( self.bitNum):
-                    if j < crossoverPonint:
-                        for k in range(self.tournamentSize):
-                            tmpStrLi[k] += inputArr[tmpPairLi[k]][j]
-                    else: #後段交換
-                        for k in range(self.tournamentSize):#輪換
-                            tmpStrLi[k] += inputArr[tmpPairLi[k+1 if k+1 < self.tournamentSize else 0]][j]
+                #Crossover
+                for k in range(self.tournamentSize):
+                    tmpStrLi[k] += inputArr[tmpPairLi[k]][0:crossoverPonint]
+#                    else: #後段交換
+                    #輪換，為了維持可以多組交配
+                    tmpStrLi[k] += inputArr[tmpPairLi[k+1 if k+1 < self.tournamentSize else 0]][crossoverPonint:]
+                #儲存
                 if self.__wheelGetDiffPopOnlyTF__:
                     #如果從頭儲存，無法看變化，所以就按照位置吧；
                     for k in range(self.tournamentSize):
                         newLi[tmpPairLi[k]] = tmpStrLi[k]
                 else:
-                    #要保留舊有的，而且同位置可能有兩組
+                    #要保留舊有的，而且同位置可能有兩組，所以直接尾綴。
                     for k in range(self.tournamentSize):
                         newLi.append(tmpStrLi[k])
             else:
-                #沒配對要換掉配對內容變 -1 ，最後統一處理
+                #沒配對要踢掉配對內容，最後統一處理
                 for k in range(self.tournamentSize):
                     pairGroup[tmpPairLi[k]].remove(i)
-        
 #        print('Crossover','pairGroup', pairGroup)
         #處理沒有crossover的
         if self.__wheelGetDiffPopOnlyTF__:
@@ -152,11 +154,10 @@ class GeneticAlgorithm():
             for k, pairNumLi in enumerate(pairGroup):
                 if len(pairNumLi) == 0:
                     newLi.append(inputArr[k])
-        
 #        print('Crossover','out-', newLi)
         return np.array(newLi)
     def Mutation(self, inputStr):
-        """變異"""
+        """變異"""        
         newStr = ''
         for temp in inputStr:
             ranTemp = random.random()
@@ -200,29 +201,15 @@ class GeneticAlgorithm():
 #            print('after Mutation:',strArr,'\n\n')
         print('總子代數量:',len(strArr))
         return self.recordFitnessMax
-    def BitStringStatistic(self, amount = 10000):
-        maxF, minF = -100, 100 #fitness
-        maxS, minS = '', ''    # value x'
-        for i in range(10000):
-            bitStr = test.GenerateBitString()
-            val = test.CalCorrespondValue(bitStr)
-            fitne = test.FitnessFunc(val)
-            if fitne > maxF:
-                maxF = fitne
-                maxS = bitStr
-            if fitne < minF:
-                minF = fitne
-                minS = bitStr
-        print('max:', (maxS, maxF), 'min:', (minS, minF))
-        return
 if __name__ == '__main__' :
     import time
     startTime = time.time()
     print('START')
     test = GeneticAlgorithm()
-    test.BitStringStatistic()
-#    ans = test.MainFlow()
-#    print('Final::', 'String:',ans[0], 'Value(x):', round(ans[1],5), 'Fitness:', round(ans[2], 5))
+    ans = test.MainFlow()
+    print('Final::', 'String:',ans[0], 'Value(x):', round(ans[1],5), 'Fitness:', round(ans[2], 5))
+#    print(test.Mutation('101'))
+    
     
     endTime = time.time()
     print('\n\n\nEND,', 'It takes', endTime-startTime ,'sec.')
